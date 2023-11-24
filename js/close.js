b9ae6734690a3f4258e55a4d0bcacff8ccc23783
kermitjestem <109726913+kermitjestem@users.noreply.github.com>
2023-11-24 01:02:16 +0100

    Add files via upload
    work in progress
    
    html/scss/js
    
    next step - connect with mongoDb

---
+++ js/close.js
@@ -0,0 +1,9 @@
+function windowClose() {
+
+    document.getElementById("login-active").style.display = "none"
+    
+    document.getElementById("register-active").style.display = "none"
+    document.getElementById("logo").style.display = "flex"
+    document.getElementById("options").style.display = "flex"
+
+}
---
+++ js/login.js
@@ -0,0 +1,15 @@
+function login () {
+  
+
+    document.getElementById("logo").style.display = "none";
+    document.getElementById("options").style.display = "none";
+    document.getElementById("login-active").style.display = "flex";
+}
+
+function register () {
+  
+
+    document.getElementById("logo").style.display = "none";
+    document.getElementById("options").style.display = "none";
+    document.getElementById("register-active").style.display = "flex";
+}
